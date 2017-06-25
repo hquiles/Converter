fa9317a41f65f4f2ea15e166a83e8666a6c9103b
hectorquiles <lafamiliaquiles@gmail.com>
2017-06-24 23:47:37 -0400

    Save Progress
    Save Progress

---
+++ Converter/Converter/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://code.msdn.microsoft.com/office/How-to-convert-excel-file-7a9bb404 
+
+            using (SpreadsheetDocument spreadSheetDocument = SpreadsheetDocument.Open(@"D:\my desktop\temp\GoogleTests.xlsx", false))
+            {
+                WorkbookPart wbPart = spreadSheetDocument.WorkbookPart;
+
+                OpenXmlReader reader = OpenXmlReader.Create(wbPart);
+
+                while (reader.Read())
+                {
+                    if (reader.ElementType == typeof(Sheet))
+                    {
+                        Sheet sheet = (Sheet)reader.LoadCurrentElement();
+
+                        WorksheetPart wsPart = (WorksheetPart)(wbPart.GetPartById(sheet.Id));
+
+                        OpenXmlReader wsReader = OpenXmlReader.Create(wsPart);
+                        while (wsReader.Read())
+                        {
+                            if (wsReader.ElementType == typeof(Worksheet))
+                            {
+                                Worksheet wsPartXml = (Worksheet)wsReader.LoadCurrentElement();
+                                //Console.WriteLine(wsPartXml.OuterXml + "\n");
+                                Console.WriteLine(wsPartXml.InnerXml);
+                            }
+                        }
+                    }
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
